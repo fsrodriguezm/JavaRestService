586520c51b6270720ee06ef93e06636e15f352d9
Felipe Rodriguez <surfer_samu@hotmail.com>
2017-07-28 13:05:40 -0400

    Refractor class name

--- src/de/jay/jersey/first/JavaRestService.java
+++ src/de/jay/jersey/first/JavaRestService.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class HelloWorldRestService {
+public class JavaRestService {
 // This method is called if TEXT_PLAIN is request
     @GET
     @Produces(MediaType.TEXT_PLAIN)
